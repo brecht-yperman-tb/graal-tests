a36f61225cc3316ba1a14d71da5c5c0e29f30740
Brecht Yperman <brecht.yperman@trustbuilder.com>
2021-08-19 10:55:48 +0200

    Initial commit

---
+++ src/test/java/TestGraalMap.java
@@ -0,0 +1,24 @@
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.TypeLiteral;
+import org.graalvm.polyglot.Value;
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TestGraalMap {
+    static String JS_CODE = "(function myFun(){ return { listProperty: ['listValue']};})";
+
+    @Test
+    public void testList() {
+        try (Context context = Context.create()) {
+            Value value = context.eval("js", JS_CODE);
+            Value result = value.execute();
+            Map<String,Object> resultMap = result.as(Map.class);
+            assertThat(resultMap).hasEntrySatisfying("listProperty", testArray -> {
+                assertThat(testArray).asList().containsExactly("listValue");
+            });
+        }
+    }
+}
